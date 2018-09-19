78619d9ad12da2ae968a7fc6304824b7f02ba8f5
Phillip Oertel <poertel@google.com>
2018-09-19 13:39:31 -0700

    In deduce.go, only show meta when failing to parse

--- gps/deduce.go
+++ gps/deduce.go
@@ -877,9 +877,8 @@ func getMetadata(ctx context.Context, path, scheme string) (string, string, stri
 	tee := io.TeeReader(rc, buf)
 
 	imports, err := parseMetaGoImports(tee)
-	log.Printf("Metadata for dependency %s is:\n%s\n", path, buf.String())
 	if err != nil {
-		return "", "", "", errors.Wrapf(err, "unable to parse go-import metadata")
+		return "", "", "", errors.Wrapf(err, "unable to parse go-import metadata\n%s", buf.String())
 	}
 	match := -1
 	for i, im := range imports {
